33bf57bbf0564ef4d0131e7a058b2c18989daba7
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-08-17 20:55:38 -0300

    feat: :fire: Remove componentes o MUI que foram colcoados para teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { TextField } from "@mui/material";
+import { Card, TextField } from "@mui/material";
 
 export default function Home() {
     return (
         <main className="h-screen p-5">
             <h1>Olá mundo</h1>
-            <TextField></TextField>
         </main>
     );
 }
