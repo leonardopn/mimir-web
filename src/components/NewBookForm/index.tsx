26384b3748168815179f1e05a1b7f0722f9e6dbd
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-08-17 22:33:44 -0300

    feat(Component: NewBookForm): :sparkles: Começa a implementação do formulário de criação

--- src/components/NewBookForm/index.tsx
+++ src/components/NewBookForm/index.tsx
@@ -1,7 +1,46 @@
 "use client";
 
-interface NewBookFormProps {}
+import { RHFInput } from "@components/Form/RHFInput";
+import { Button } from "@mui/material";
+import { Book } from "@typings/Book";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-export function NewBookForm({}: NewBookFormProps) {
-	return <form className=""></form>;
+interface FormProps extends Omit<Book, "id" | "createdAt" | "updatedAt" | "readDate"> {}
+
+export function NewBookForm() {
+	const { control, handleSubmit } = useForm<FormProps>({
+		defaultValues: {
+			title: "",
+			description: "",
+			author: [],
+			cover: null,
+			gender: [],
+			isbn: "",
+			isbn13: "",
+			publishDate: null,
+			publisher: "",
+			tags: [],
+		},
+	});
+
+	const onSubmit: SubmitHandler<FormProps> = data => {
+		console.log(data);
+	};
+
+	return (
+		<form className="grid grid-cols-1 gap-4" onSubmit={handleSubmit(onSubmit)}>
+			<RHFInput control={control} name="title" label="Título"></RHFInput>
+			<RHFInput control={control} name="isbn" label="ISBN"></RHFInput>
+			<RHFInput control={control} name="isbn13" label="ISBN13"></RHFInput>
+			<RHFInput
+				control={control}
+				name="description"
+				multiline
+				minRows={3}
+				label="Descrição"></RHFInput>
+			<Button type="submit" variant="contained">
+				Salvar
+			</Button>
+		</form>
+	);
 }
