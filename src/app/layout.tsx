dd97f296f626d3e9a0adfe55f3c056349986d50d
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-08-17 22:01:10 -0300

    feat: :sparkles: Adiciona uma estilização de cor e padding padronizada em todo o app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,21 +5,23 @@ import { ReactNode } from "react";
 import ThemeRegistry from "../theme/ThemeRegistry";
 
 const roboto = Roboto({
-    subsets: ["latin"],
-    weight: ["300", "400", "500", "700"],
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "700"],
 });
 
 export const metadata: Metadata = {
-    title: "My Finance",
-    description: "Gerenciei suas finanças de forma simples e prática.",
+	title: "My Finance",
+	description: "Gerenciei suas finanças de forma simples e prática.",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-    return (
-        <html lang="pt-BR">
-            <body className={roboto.className} id="__next">
-                <ThemeRegistry options={{ key: "css" }}>{children}</ThemeRegistry>
-            </body>
-        </html>
-    );
+	return (
+		<html lang="pt-BR">
+			<body className={roboto.className} id="__next">
+				<main className="h-screen p-5 bg-slate-200">
+					<ThemeRegistry options={{ key: "css" }}>{children}</ThemeRegistry>
+				</main>
+			</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { Card, TextField } from "@mui/material";
-
 export default function Home() {
-    return (
-        <main className="h-screen p-5">
-            <h1>Olá mundo</h1>
-        </main>
-    );
+	return (
+		<div>
+			<h1>Olá mundo</h1>
+		</div>
+	);
 }
