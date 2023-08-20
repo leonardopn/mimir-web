5ed36ace7b4cb96039aa1bb866c3f995fe969eb1
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-08-20 16:05:06 -0300

    feat(Component: BooksPageList): :sparkles: Cria o componente inicialmente

---
+++ src/components/BooksPageList/index.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { BookCover } from "@components/BookCover";
+import { useBooks } from "@hooks/useBooks";
+
+interface BooksPageListProps {}
+
+export function BooksPageList({}: BooksPageListProps) {
+	const { data } = useBooks();
+	return (
+		<div className="flex gap-8 flex-wrap justify-center w-full">
+			{data?.map(book => (
+				<BookCover src={book.cover} alt={book.title + " cover"} key={book.id}></BookCover>
+			))}
+		</div>
+	);
+}
