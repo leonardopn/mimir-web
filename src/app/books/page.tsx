fbbe1c396bb34823e81d7851ec8fb605cae1181e
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-08-20 16:05:44 -0300

    feat(Page: Books): :sparkles: Exibie os livros na store na página

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,5 +1,10 @@
+import { BooksPageList } from "@components/BooksPageList";
 import { Container } from "@mui/material";
 
 export default function Books() {
-	return <Container className="max-w-screen-md flex items-center p-0">Olá mundo</Container>;
+	return (
+		<Container className="max-w-screen-xl flex items-center p-0">
+			<BooksPageList></BooksPageList>
+		</Container>
+	);
 }
