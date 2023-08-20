93bf656b0f2d57a2f85367421f3975b8fafc684e
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-08-20 16:33:14 -0300

    chore: :lipstick: Adiciona  nova cor primary

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    important: "#__next",
-    theme: {
-        fontFamily: {
-            sans: ["Roboto", "Arial", "sans-serif"],
-        },
-        extend: {
-            colors: {
-                background: {
-                    light: "#f8f8f2",
-                    main: "#282a36",
-                    dark: "#282a36",
-                },
-                background2: {
-                    light: "#f8f8f2",
-                    main: "#44475a",
-                    dark: "#44475a",
-                },
-                textColor: {
-                    light: "#282a36",
-                    main: "#f8f8f2",
-                    dark: "#f8f8f2",
-                },
-                success: "#50fa7b",
-                error: "#ff5555",
-                purple: "#bd93f9",
-                orange: "#ffb86c",
-            },
-        },
-    },
-    corePlugins: {
-        preflight: false,
-    },
-    plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	important: "#__next",
+	theme: {
+		fontFamily: {
+			sans: ["Roboto", "Arial", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				primary: "#550044",
+				background: {
+					light: "#f8f8f2",
+					main: "#282a36",
+					dark: "#282a36",
+				},
+				background2: {
+					light: "#f8f8f2",
+					main: "#44475a",
+					dark: "#44475a",
+				},
+				textColor: {
+					light: "#282a36",
+					main: "#f8f8f2",
+					dark: "#f8f8f2",
+				},
+				success: "#50fa7b",
+				error: "#ff5555",
+				purple: "#bd93f9",
+				orange: "#ffb86c",
+			},
+		},
+	},
+	corePlugins: {
+		preflight: false,
+	},
+	plugins: [],
 };
