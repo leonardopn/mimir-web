0b9e600d688ce401b8f3af0338897a5a8051b014
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-08-20 20:22:18 -0300

    feat(Component: BookCover): :sparkles: Permite usar ou não um link como wrapper

--- src/components/BookCover/index.tsx
+++ src/components/BookCover/index.tsx
@@ -5,21 +5,24 @@ import Link from "next/link";
 interface BookCoverProps {
 	data: Book;
 	alt?: string;
+	disableLink?: boolean;
 }
 
-export function BookCover({ data, alt }: BookCoverProps) {
+export function BookCover({ data, alt, disableLink }: BookCoverProps) {
 	const srcToUse = data.cover || "/book-cover-placeholder.png";
 
-	return (
-		<Link href={`/books/${data.id}`}>
-			<div className="cursor-pointer">
-				<Image
-					src={srcToUse}
-					alt={alt || "capa do livro"}
-					width={250}
-					height={400}
-					className="rounded-xl shadow-xl hover:scale-105 transition-transform"></Image>
-			</div>
-		</Link>
+	const content = (
+		<Image
+			src={srcToUse}
+			alt={alt || "capa do livro"}
+			width={250}
+			height={400}
+			className="rounded-xl shadow-xl hover:scale-105 transition-transform cursor-pointer"></Image>
 	);
+
+	if (disableLink) {
+		return content;
+	}
+
+	return <Link href={`/books/${data.id}`}>{content}</Link>;
 }
