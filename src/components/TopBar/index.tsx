3d325866bcaab8de7ebf437282dfe6a7966b226b
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-08-20 16:33:57 -0300

    feat(Component: TopBar): :sparkles: Cria o componente

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import LogoSVG from "../../../public/imgs/logo.svg";
+import Link from "next/link";
+
+interface TopBarProps {}
+
+export function TopBar({}: TopBarProps) {
+	return (
+		<div className="bg-primary w-full h-16 flex items-center px-4 shadow-xl">
+			<Link href="/">
+				<Image
+					src={LogoSVG}
+					alt="mimir logo"
+					width={40}
+					height={40}
+					className="hover:opacity-60 transition-opacity cursor-pointer active:opacity-100"></Image>
+			</Link>
+		</div>
+	);
+}
