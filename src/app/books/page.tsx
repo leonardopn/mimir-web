34a0446802d46f147db24f2ceeccf85e213d93c3
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-08-20 15:26:35 -0300

    feat(Page: Books): :sparkles: Cria a página books

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,12 +1,5 @@
-import { NewBookForm } from "@components/NewBookForm";
-import { Card, Container } from "@mui/material";
+import { Container } from "@mui/material";
 
 export default function Books() {
-	return (
-		<Container className="max-w-screen-md flex items-center p-0">
-			<Card className="p-4 w-full">
-				<NewBookForm></NewBookForm>
-			</Card>
-		</Container>
-	);
+	return <Container className="max-w-screen-md flex items-center p-0">Olá mundo</Container>;
 }
