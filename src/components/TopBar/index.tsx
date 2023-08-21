ca1aa88c330c450c6976c4bfe07e94f7a003e766
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-08-21 20:27:48 -0300

    feat(Component: TopBar): :lipstick: Ajusta justify e o tamanho do padding x

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -6,15 +6,17 @@ interface TopBarProps {}
 
 export function TopBar({}: TopBarProps) {
 	return (
-		<div className="bg-primary w-full h-16 flex items-center px-4 shadow-xl">
-			<Link href="/">
-				<Image
-					src={LogoSVG}
-					alt="mimir logo"
-					width={40}
-					height={40}
-					className="hover:opacity-60 transition-opacity cursor-pointer active:opacity-100"></Image>
-			</Link>
+		<div className="bg-primary w-full h-16 flex items-center px-20 justify-center shadow-xl">
+			<main>
+				<Link href="/">
+					<Image
+						src={LogoSVG}
+						alt="mimir logo"
+						width={40}
+						height={40}
+						className="hover:opacity-60 transition-opacity cursor-pointer active:opacity-100"></Image>
+				</Link>
+			</main>
 		</div>
 	);
 }
