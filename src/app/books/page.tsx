296bd1699be8f3b206a8bcc717805b070cabf448
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-08-21 20:30:41 -0300

    feat(Page: Books): :sparkles: Adiciona meios de navegar a tela de criação de livro pela listagem

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,10 +1,12 @@
 import { BooksPageList } from "@components/BooksPageList";
+import { FloatingAddButton } from "@components/FloatingAddButton";
 import { Container } from "@mui/material";
 
 export default function Books() {
 	return (
 		<Container className="max-w-screen-xl flex items-center">
 			<BooksPageList></BooksPageList>
+			<FloatingAddButton tip="Adicionar livro" link="/books/new"></FloatingAddButton>
 		</Container>
 	);
 }
