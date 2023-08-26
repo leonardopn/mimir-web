3c7d9271cb916a46c1638b18f95c35a452ce243c
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-08-26 18:11:09 -0300

    feat(Slice: Books): :sparkles: Implementa novos recursos no slice
    Adiciona 3 novos campos, além de re-organziar internamente o slice. Agora temos como rastrear o estado de busca, se já foi carregado os dados, se foi tentado carregar dados e a própria função de carregar dados

--- src/@types/Store/index.d.ts
+++ src/@types/Store/index.d.ts
@@ -1,8 +1,20 @@
 import { Book } from "../Book/index";
 
-export interface BooksSliceState {
+export interface ISliceState {
+	isFetching: boolean;
+	setIsFetching: (isFetching: boolean) => void;
+
+	initialFetchWasMade: boolean;
+	setInitialFetchWasMade: (initialFetchWasMade: boolean) => void;
+
+	initialStateHasBeenLoaded: boolean;
+	setInitialStateHasBeenLoaded: (initialStateHasBeenLoaded: boolean) => void;
+}
+
+export interface BooksSliceState extends ISliceState {
 	data: Book[];
 	addBook: (data: Book) => void;
+	fetchBooks: () => Promise<void>;
 }
 
 export interface StoreState {
--- src/store/Books/index.ts
+++ src/store/Books/index.ts
@@ -3,8 +3,53 @@ import { BooksSliceState, StoreState } from "@typings/Store";
 import update from "immutability-helper";
 import { StateCreator } from "zustand";
 import { mockBooks } from "../../mock/books";
+import { BookService } from "../../services/firebase/Book";
 
-export const BooksSlice: StateCreator<StoreState, [], [], BooksSliceState> = set => ({
-	data: [...mockBooks],
-	addBook: book => set(state => update(state, { books: { data: { $push: [book] } } })),
-});
+export const BooksSlice: StateCreator<StoreState, [], [], BooksSliceState> = set => {
+	const addBook = (book: Book) =>
+		set(state => update(state, { books: { data: { $push: [book] } } }));
+
+	const setInitialFetchWasMade = (initialFetchWasMade: boolean) =>
+		set(state =>
+			update(state, { books: { initialFetchWasMade: { $set: initialFetchWasMade } } })
+		);
+
+	const setInitialStateHasBeenLoaded = (initialStateHasBeenLoaded: boolean) =>
+		set(state =>
+			update(state, {
+				books: { initialStateHasBeenLoaded: { $set: initialStateHasBeenLoaded } },
+			})
+		);
+
+	const setIsFetching = (isFetching: boolean) => {
+		set(state => update(state, { books: { isFetching: { $set: isFetching } } }));
+	};
+
+	const fetchBooks = async () => {
+		try {
+			setIsFetching(true);
+			setInitialFetchWasMade(true);
+
+			const books = await BookService.getAll();
+
+			set(state => update(state, { books: { data: { $set: books } } }));
+			setInitialStateHasBeenLoaded(true);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setIsFetching(false);
+		}
+	};
+
+	return {
+		data: [...mockBooks],
+		initialFetchWasMade: false,
+		initialStateHasBeenLoaded: false,
+		isFetching: false,
+		addBook,
+		setIsFetching,
+		setInitialFetchWasMade,
+		setInitialStateHasBeenLoaded,
+		fetchBooks,
+	};
+};
