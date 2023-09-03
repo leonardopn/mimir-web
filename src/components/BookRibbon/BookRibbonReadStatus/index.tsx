af27142c800ba60033e0113f5e6ed68e2b89f2ba
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-09-03 16:42:50 -0300

    feat(Component: BookRibbonReadStatus): :sparkles: Cria o componente

---
+++ src/components/BookRibbon/BookRibbonReadStatus/index.tsx
@@ -0,0 +1,24 @@
+import dayjs from "dayjs";
+import { BookRibbon } from "..";
+import { Tooltip } from "@mui/material";
+import { Icon } from "@iconify/react";
+
+interface BookRibbonReadStatusProps {
+	readDate: Date | null;
+}
+
+export function BookRibbonReadStatus({ readDate }: BookRibbonReadStatusProps) {
+	const tooltipText = readDate
+		? `Lido em: ${dayjs(readDate).format("ddd, DD MMM YYYY")}`
+		: "Não lido";
+	const icon = readDate ? "mdi:check-decagram" : "mdi:alert-decagram";
+	const color: [string, string] | undefined = readDate ? undefined : ["bg-red-500", "bg-red-600"];
+
+	return (
+		<BookRibbon color={color}>
+			<Tooltip title={tooltipText} arrow>
+				<Icon icon={icon} className="text-white h-5 w-5 mb-2" />
+			</Tooltip>
+		</BookRibbon>
+	);
+}
