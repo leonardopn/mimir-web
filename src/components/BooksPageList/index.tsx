704999371412e45f8d761ef0fb69b1549c18feef
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-09-03 14:39:11 -0300

    feat(Component: BooksPageList): :sparkles: Adiciona um loading enquanto não há livro

--- src/components/BooksPageList/index.tsx
+++ src/components/BooksPageList/index.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import { BookCover } from "@components/BookCover";
+import { LoadingFullPage } from "@components/LoadingFullPage";
 import { useBooks } from "@hooks/useBooks";
+import { useMemo } from "react";
 
 interface BooksPageListProps {}
 
 export function BooksPageList({}: BooksPageListProps) {
-	const { data } = useBooks();
+	const { data, isFetching } = useBooks();
+
+	if (isFetching) return <LoadingFullPage />;
+
 	return (
 		<div className="flex gap-8 flex-wrap justify-center w-full">
-			{data?.map(book => (
+			{data.map(book => (
 				<BookCover data={book} alt={book.title + " cover"} key={book.id}></BookCover>
 			))}
 		</div>
