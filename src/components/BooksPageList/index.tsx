303d859708425d7d76d97831f72d9f973f79ce87
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-09-04 23:47:54 -0300

    feat: :sparkles: Move o busca de livro para a TopBar

--- src/components/BooksPageList/index.tsx
+++ src/components/BooksPageList/index.tsx
@@ -3,36 +3,18 @@
 import { BookCover } from "@components/BookCover";
 import { LoadingFullPage } from "@components/LoadingFullPage";
 import { useBooks } from "@hooks/useBooks";
-import { Card, TextField } from "@mui/material";
-import { debounce, filter } from "lodash";
-import { useState } from "react";
+import { useFilterBooksContext } from "@hooks/useFilterBooksContext";
 
 interface BooksPageListProps {}
 
 export function BooksPageList({}: BooksPageListProps) {
-	const { data, isFetching } = useBooks();
-
-	const [filteredBooks, setFilteredBooks] = useState(data);
-
-	const debouncedSearch = debounce(text => {
-		const result = text
-			? filter(data, book => {
-					return book.title.toLowerCase().includes(text);
-			  })
-			: data;
-		setFilteredBooks(result);
-	}, 800);
+	const { filteredBooks } = useFilterBooksContext();
+	const { isFetching } = useBooks();
 
 	if (isFetching) return <LoadingFullPage />;
 
 	return (
 		<div className="flex flex-col w-full gap-8">
-			<Card>
-				<TextField
-					className="w-full"
-					placeholder="Pesquisei aqui..."
-					onChange={e => debouncedSearch(e.target.value)}></TextField>
-			</Card>
 			<main className="flex gap-8 flex-wrap justify-center w-full">
 				{filteredBooks.map(book => (
 					<BookCover data={book} alt={book.title + " cover"} key={book.id}></BookCover>
--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,22 +1,22 @@
 import Image from "next/image";
 import LogoSVG from "../../../public/imgs/logo.svg";
 import Link from "next/link";
+import { SearchBookBar } from "@components/SearchBookBar";
 
 interface TopBarProps {}
 
 export function TopBar({}: TopBarProps) {
 	return (
-		<div className="bg-primary w-full h-16 flex items-center px-20 justify-center shadow-xl">
-			<main>
-				<Link href="/">
-					<Image
-						src={LogoSVG}
-						alt="mimir logo"
-						width={40}
-						height={40}
-						className="hover:opacity-60 transition-opacity cursor-pointer active:opacity-100"></Image>
-				</Link>
-			</main>
+		<div className="bg-primary w-full h-16 flex items-center px-20 justify-between shadow-xl">
+			<Link href="/">
+				<Image
+					src={LogoSVG}
+					alt="mimir logo"
+					width={40}
+					height={40}
+					className="hover:opacity-60 transition-opacity cursor-pointer active:opacity-100"></Image>
+			</Link>
+			<SearchBookBar data={[]}></SearchBookBar>
 		</div>
 	);
 }
