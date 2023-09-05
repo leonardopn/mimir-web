4ec72f7a484732af57ce27f57660ad829208ca82
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-09-04 23:50:50 -0300

    fix(Component: BooksPageList): :ambulance: Corrige um bug de importação de arquivo inexistente

--- src/components/BooksPageList/index.tsx
+++ src/components/BooksPageList/index.tsx
@@ -3,20 +3,18 @@
 import { BookCover } from "@components/BookCover";
 import { LoadingFullPage } from "@components/LoadingFullPage";
 import { useBooks } from "@hooks/useBooks";
-import { useFilterBooksContext } from "@hooks/useFilterBooksContext";
 
 interface BooksPageListProps {}
 
 export function BooksPageList({}: BooksPageListProps) {
-	const { filteredBooks } = useFilterBooksContext();
-	const { isFetching } = useBooks();
+	const { data, isFetching } = useBooks();
 
 	if (isFetching) return <LoadingFullPage />;
 
 	return (
 		<div className="flex flex-col w-full gap-8">
 			<main className="flex gap-8 flex-wrap justify-center w-full">
-				{filteredBooks.map(book => (
+				{data.map(book => (
 					<BookCover data={book} alt={book.title + " cover"} key={book.id}></BookCover>
 				))}
 			</main>
