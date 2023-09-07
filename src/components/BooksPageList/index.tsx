58cb7b9fa563cd36803dea8e04e4cd537c25a97c
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-09-07 15:47:41 -0300

    feat(Component: BooksPageList): :sparkles: Implementa a filtragem de livros baseado no SearchSlice

--- src/components/BooksPageList/index.tsx
+++ src/components/BooksPageList/index.tsx
@@ -3,18 +3,32 @@
 import { BookCover } from "@components/BookCover";
 import { LoadingFullPage } from "@components/LoadingFullPage";
 import { useBooks } from "@hooks/useBooks";
+import { useSearch } from "@hooks/useSearch";
+import { filter, map } from "lodash";
+import { useMemo } from "react";
 
 interface BooksPageListProps {}
 
 export function BooksPageList({}: BooksPageListProps) {
+	const { data: textToSearch } = useSearch();
 	const { data, isFetching } = useBooks();
+
+	const filteredBooks = useMemo(() => {
+		if (!textToSearch) return data;
+
+		const textUpper = textToSearch.toUpperCase();
+
+		return filter(data, book => {
+			return book.title.toUpperCase().includes(textUpper);
+		});
+	}, [data, textToSearch]);
 
 	if (isFetching) return <LoadingFullPage />;
 
 	return (
 		<div className="flex flex-col w-full gap-8">
 			<main className="flex gap-8 flex-wrap justify-center w-full">
-				{data.map(book => (
+				{filteredBooks.map(book => (
 					<BookCover data={book} alt={book.title + " cover"} key={book.id}></BookCover>
 				))}
 			</main>
