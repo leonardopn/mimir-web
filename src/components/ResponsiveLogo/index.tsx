afbbb2bcab5ba9122b6674f07e949543e76f6211
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-09-07 22:57:58 -0300

    feat(Component: ResponsiveLogo): :sparkles: Melhora o componente para evitar problemas de performance com o next

--- src/components/ResponsiveLogo/index.tsx
+++ src/components/ResponsiveLogo/index.tsx
@@ -1,32 +1,14 @@
-"use client";
-
-import { useResponsive } from "@hooks/useResponsive";
+import Image from "next/image";
 import AppNameSVG from "../../../public/imgs/app_name.svg";
 import LogoSVG from "../../../public/imgs/logo.svg";
-import Image from "next/image";
 
-interface ResponsiveLogoProps {}
-
-export function ResponsiveLogo({}: ResponsiveLogoProps) {
-	const isSm = useResponsive("sm");
-
-	if (!isSm) {
-		return (
-			<Image
-				src={AppNameSVG}
-				alt="app_name"
-				width={80}
-				height={50}
-				className="cursor-pointer active:opacity-100"></Image>
-		);
-	}
-
+export function ResponsiveLogo() {
 	return (
-		<Image
-			src={LogoSVG}
-			alt="mimir_logo"
-			width={40}
-			height={40}
-			className="cursor-pointer active:opacity-100"></Image>
+		<div className="h-10 sm:w-20 w-10 relative cursor-pointer active:opacity-100">
+			<picture>
+				<source media="(min-width: 640px)" srcSet={AppNameSVG.src} />
+				<Image src={LogoSVG} alt="logo" fill />
+			</picture>
+		</div>
 	);
 }
