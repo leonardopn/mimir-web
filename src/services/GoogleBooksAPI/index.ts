60f3f9df5c5846966f1937445c7b8fb335356457
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-09-09 22:37:23 -0300

    refactor(Service: GoogleBooksAPI): :label: Torna todos os campos de IGoogleBooksApi opcionais
    Feito para evitar problemas com tipagem externa desconhecida

--- src/services/GoogleBooksAPI/index.ts
+++ src/services/GoogleBooksAPI/index.ts
@@ -1,103 +1,113 @@
 import axios from "axios";
 
 export interface IGoogleBooksApi {
-	kind: "books#volume";
-	id: string;
-	etag: string;
-	selfLink: string;
-	volumeInfo: {
-		title: string;
-		subtitle: string;
+	kind?: "books#volume";
+	id?: string;
+	etag?: string;
+	selfLink?: string;
+	volumeInfo?: {
+		title?: string;
+		subtitle?: string;
 		authors?: string[];
-		publisher: string;
-		publishedDate: string;
-		description: string;
-		industryIdentifiers?: [
-			{
-				type: string;
-				identifier: string;
-			}
-		];
-		pageCount: number;
-		dimensions: {
-			height: string;
-			width: string;
-			thickness: string;
+		publisher?: string;
+		publishedDate?: string;
+		description?: string;
+		industryIdentifiers?: {
+			type?: string;
+			identifier?: string;
+		}[];
+		readingModes?: {
+			text?: boolean;
+			image?: boolean;
 		};
-		printType: string;
-		mainCategory: string;
-		categories: [string];
-		averageRating: number;
-		ratingsCount: number;
-		contentVersion: string;
-		imageLinks: {
-			smallThumbnail: string;
-			thumbnail: string;
-			small: string;
-			medium: string;
-			large: string;
-			extraLarge: string;
+		maturityRating?: string;
+		allowAnonLogging?: boolean;
+		pageCount?: number;
+		dimensions?: {
+			height?: string;
+			width?: string;
+			thickness?: string;
 		};
-		language: string;
-		previewLink: string;
-		infoLink: string;
-		canonicalVolumeLink: string;
+		printType?: string;
+		mainCategory?: string;
+		categories?: [string];
+		averageRating?: number;
+		ratingsCount?: number;
+		contentVersion?: string;
+		panelizationSummary?: {
+			containsEpubBubbles?: boolean;
+			containsImageBubbles?: boolean;
+		};
+		imageLinks?: {
+			smallThumbnail?: string;
+			thumbnail?: string;
+			small?: string;
+			medium?: string;
+			large?: string;
+			extraLarge?: string;
+		};
+		language?: string;
+		previewLink?: string;
+		infoLink?: string;
+		canonicalVolumeLink?: string;
 	};
-	userInfo: {
-		isPurchased: boolean;
-		isPreordered: boolean;
-		updated: Date;
+	userInfo?: {
+		isPurchased?: boolean;
+		isPreordered?: boolean;
+		updated?: Date;
 	};
-	saleInfo: {
-		country: string;
-		saleability: string;
-		onSaleDate: Date;
-		isEbook: boolean;
-		listPrice: {
-			amount: number;
-			currencyCode: string;
+	saleInfo?: {
+		country?: string;
+		saleability?: string;
+		onSaleDate?: Date;
+		isEbook?: boolean;
+		listPrice?: {
+			amount?: number;
+			currencyCode?: string;
 		};
-		retailPrice: {
-			amount: number;
-			currencyCode: string;
+		retailPrice?: {
+			amount?: number;
+			currencyCode?: string;
 		};
-		buyLink: string;
+		buyLink?: string;
+		offers?: any[];
 	};
-	accessInfo: {
-		country: string;
-		viewability: string;
-		embeddable: boolean;
-		publicDomain: boolean;
-		textToSpeechPermission: string;
-		epub: {
-			isAvailable: boolean;
-			downloadLink: string;
-			acsTokenLink: string;
+	accessInfo?: {
+		country?: string;
+		viewability?: string;
+		embeddable?: boolean;
+		publicDomain?: boolean;
+		textToSpeechPermission?: string;
+		epub?: {
+			isAvailable?: boolean;
+			downloadLink?: string;
+			acsTokenLink?: string;
 		};
-		pdf: {
-			isAvailable: boolean;
-			downloadLink: string;
-			acsTokenLink: string;
+		pdf?: {
+			isAvailable?: boolean;
+			downloadLink?: string;
+			acsTokenLink?: string;
 		};
-		webReaderLink: string;
-		accessViewStatus: string;
-		downloadAccess: {
-			kind: "books#downloadAccessRestriction";
-			volumeId: string;
-			restricted: boolean;
-			deviceAllowed: boolean;
-			justAcquired: boolean;
-			maxDownloadDevices: number;
-			downloadsAcquired: number;
-			nonce: string;
-			source: string;
-			reasonCode: string;
-			message: string;
-			signature: string;
+		webReaderLink?: string;
+		quoteSharingAllowed?: boolean;
+		accessViewStatus?: string;
+		downloadAccess?: {
+			kind?: "books#downloadAccessRestriction";
+			volumeId?: string;
+			restricted?: boolean;
+			deviceAllowed?: boolean;
+			justAcquired?: boolean;
+			maxDownloadDevices?: number;
+			downloadsAcquired?: number;
+			nonce?: string;
+			source?: string;
+			reasonCode?: string;
+			message?: string;
+			signature?: string;
 		};
 	};
-	searchInfo: {
-		textSnippet: string;
+	searchInfo?: {
+		textSnippet?: string;
 	};
 }
 
