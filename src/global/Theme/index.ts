381602a86ece838b7203d256ae3431018ecfb2ea
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-12-22 14:37:58 -0300

    feat: :lipstick: Atualiza o tema
    Cria uma paleta de cores para a cor primŕia do app

--- src/global/Theme/index.ts
+++ src/global/Theme/index.ts
@@ -1,8 +1,28 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, withDefaultProps } from "@chakra-ui/react";
 
-export const theme = extendTheme({
-	fonts: {
-		heading: "var(--font-roboto)",
-		body: "var(--font-roboto)",
+export const theme = extendTheme(
+	{
+		colors: {
+			primary: {
+				100: "#9f4c8f",
+				200: "#822f71",
+				300: "#741a62",
+				400: "#61094f",
+				500: "#550044",
+				600: "#3f0032",
+				700: "#2e0025",
+				800: "#26001e",
+				900: "#200019",
+			},
+		},
+		fonts: {
+			heading: "var(--font-roboto)",
+			body: "var(--font-roboto)",
+		},
 	},
-});
+	withDefaultProps({
+		defaultProps: {
+			colorScheme: "primary",
+		},
+	})
+);
