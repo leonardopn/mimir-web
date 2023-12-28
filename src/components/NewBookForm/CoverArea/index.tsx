3c0c5c45862a2cc0a6023ab32393225fbc8389dd
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2023-12-28 16:34:33 -0300

    feat(Component: CoverArea): :sparkles: Adiciona mais elementos ao componente
    Exibe uma capa padrão e um botão apra mudar a capa do livro (ainda não funcional)

--- src/components/NewBookForm/CoverArea/index.tsx
+++ src/components/NewBookForm/CoverArea/index.tsx
@@ -1,7 +1,20 @@
-import { Card } from "@chakra-ui/react";
+import { Button, Card, Heading } from "@chakra-ui/react";
+import { BookCoverDefault } from "@components/BookCoverDefault";
+import { useWatch } from "react-hook-form";
+import { NewBookFormProps } from "..";
 
 interface CoverAreaProps {}
 
 export function CoverArea({}: CoverAreaProps) {
-	return <Card className="p-4">olá mundo</Card>;
+	const { title, publisher } = useWatch<NewBookFormProps>();
+
+	return (
+		<Card className="p-4 flex flex-col items-center gap-3 h-fit lg:sticky lg:top-24 overflow-auto">
+			<Heading className="text-xl">Capa do livro</Heading>
+			<BookCoverDefault bookTitle={title} publisher={publisher} />
+			<Button className="w-full" variant="outline">
+				Mudar capa
+			</Button>
+		</Card>
+	);
 }
